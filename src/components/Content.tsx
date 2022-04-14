b448600b64072d2f9afb7b510be60c8eb9ea657c
guilherme augusto <guilherme.silva@ftdata.com>
2022-04-13 23:56:04 -0300

    refactor: separando os componentes de Sidebar e Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,44 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+}
+
+export function Content({ selectedGenreId }: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [selectedGenreId]);
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
